ee916d40bb86b2c4a7c1ce82f66ec180fa7a4d9f
unknown <muspirahumaira@gmail.com>
2024-04-15 16:31:55 +0530

    laravel_3_repo

---
+++ resources/views/main.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tech News</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+
+<body>
+  <div class="text-center">
+    <h1>News <span class="intext">Room</span></h1>
+    <h6>As of mid-April 2024, several tech trends are making headlines across industries</h6>
+    <h6>Trends span AI, quantum computing, and sustainability</h6>
+  </div>
+  </div>
+  <div class="grid-layouts container grid-con">
+    <div class="row justify-content-between row-con">
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/addiction.webp')}}" alt="SocialMedia Addiction">
+        </div>
+        <div class="description">
+          <h6>Digital Dependency:</h6>
+          <p>Navigating Social Media Addiction</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/VR.jpg')}}" alt="VR">
+        </div>
+        <div class="description">
+          <h6>Reality Redefined:</h6>
+          <p>The XR Experience</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/bitcoin.webp')}}" alt="Bitcoin">
+        </div>
+        <div class="description">
+          <h6>Decentralized Dawn:</h6>
+          <p>Blockchain and Web3 Breakthroughs</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-between row-con">
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/quantum computing.jpg')}}" alt="quantum computing">
+        </div>
+        <div class="description">
+          <h6>Quantum Leap:</h6>
+          <p>Computing's New Horizon</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/hacker.webp')}}" alt="Hacker">
+        </div>
+        <div class="description">
+          <h6>Guardians of the Net:</h6>
+          <p>Cybersecurity in the Digital Age</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/ai.jpg')}}" alt="AI">
+        </div>
+        <div class="description">
+          <h6>AI Unleashed:</h6>
+          <p>Next-Gen Intelligence</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-between row-con">
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/automation car.webp')}}" alt="Automation Car">
+        </div>
+        <div class="description">
+          <h6>Autonomous Evolution:</h6>
+          <p>The Future of Smart Transportation</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+      <div class="col-3 box-con">
+        <div>
+          <div>
+            <img class="image" src="{{asset('image/6g.jpeg')}}" alt="6G Network">
+          </div>
+          <div class="description">
+            <h6>Beyond Speed:</h6>
+            <p>The Era of 5G and 6G</p>
+            <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-3 box-con">
+        <div>
+          <img class="image" src="{{asset('image/elon in mars.webp')}}" alt="Elon In Mars">
+        </div>
+        <div class="description">
+          <h6>Mars Magnate:</h6>
+          <p>Elon Musk's Red Planet Ambitions</p>
+          <button type="submit" class="btn btn-primary stylebtn">Read Here</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
